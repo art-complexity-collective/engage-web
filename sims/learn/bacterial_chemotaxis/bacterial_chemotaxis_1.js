f86de08075cda96e486aafb45ba7165f0acd6e36
Bhaskar Kumawat <kumawatb@umich.edu>
2024-09-24 21:04:10 -0400

    Added sim 1 chemotaxis

--- sims/learn/bacterial_chemotaxis/bacterial_chemotaxis_1.js
+++ sims/learn/bacterial_chemotaxis/bacterial_chemotaxis_1.js
@@ -1,103 +1,117 @@
-import { Heatmap } from "../../modules/heatmap.js";
-import { Barplot } from "../../modules/barplot.js";
-import { Network } from "../../modules/network.js";
-
-let n_host_types = 30;
-let n_para_types = 30;
+import { AgentArena, Agent } from "../../modules/agents.js"
+import { Slider } from "../../modules/ui.js"
 
 const canvas_props = {width: 900, height: 700};
-const hmap_props = {x: 150, 
-    y: 150, 
-    width: 300, 
-    height:300, 
-    nx: n_host_types, 
-    ny: n_para_types,
-    cscale: chroma.scale()};
 
-const hplot_props = {
-    x:150,
-    y: 30,
-    width: 300,
-    height: 100,
-    domain: Array(n_host_types).keys(),
-    range: [0, 1.0],
-    angle: 0,
-    color: 'rgb(184, 81, 95)'
-}
+const arena_props = {
+    x: 50,
+    y: 5,
+    width: 800,
+    height: 600};
 
-const pplot_props = {
-    x:30,
-    y: 450,
-    width: 300,
-    height: 100,
-    domain: Array(n_para_types).keys(),
-    range: [0, 1.0],
-    angle: 90,
-    color: 'rgb(164, 218, 215)'
-}
+const trate_slider_props = {
+    x: 300,
+    y: 620,
+    height: 60,
+    width:200,
+    log: false,
+    min: 0.00,
+    max: 1.0,
+    def: 0.05,
+    step: 0.05,
+    label: "Tumble Rate"
+};
 
-const network_props = {
-    x: 500,
-    y: 50,
-    width: 200,
-    height: 500,
-    layout: {type: "bipartite", n_left: n_host_types, n_right: n_para_types}
-}
+const particle_props = {
+    x: arena_props.width/2,
+    y: arena_props.height/2,
+    height: 10,
+    width: 20,
+    angle: 45,
+    radius: 5,
+    shape: 'pointed circ',
+    color: "#000000"
+};  
 
-function normalized(pop_vec,nx,ny){
-    // TODO: WRITE CORRECT MARGINAL DENSITY FUNCTION!
-    let xvec = pop_vec.slice(0,0+nx);
-    let yvec = pop_vec.slice(0,0+ny);
-    return [xvec, yvec];
+
+class SimpleBac{
+    constructor(x=100,y=100,angle=0,v=1){
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.v = v;
+    }
+    
+    move(arena_dims){
+        this.x += this.v*Math.cos(this.angle*(Math.PI/180.0));
+        this.y += this.v*Math.sin(this.angle*(Math.PI/180.0));
+
+        if(this.x < arena_dims.x){
+            this.x = arena_dims.width+this.x;
+        } else if (this.x > arena_dims.x+arena_dims.width) {
+            this.x = this.x-arena_dims.width;
+        }
+
+        if(this.y < arena_dims.y){
+            this.y = arena_dims.height+this.y;
+        } else if (this.y > arena_dims.y+arena_dims.height) {
+            this.y = this.y - arena_dims.height;
+        }
+    }
+
+    turn_random(){
+        this.angle = Math.random() * 360;
+    }
+
+    set_velocity(value){
+        this.v = value;
+    }
+
 }
 
 const sketch = (p) => {
     
-    let hmap, hplot, pplot, intnet;
-    let pop = new Array(n_host_types*n_para_types);
-
+    let arena, trate_slider, agentid, bacteria;
 
     p.setup = () => {
         let canvas = p.createCanvas(canvas_props.width, canvas_props.height);
-        hmap = new Heatmap(p, hmap_props);
+
+        arena = new AgentArena(p, arena_props);
+        trate_slider = new Slider(p, trate_slider_props);
+
+        agentid = arena.add_agent(particle_props);
+
+        bacteria = new SimpleBac(400, 300, 45, 5);
+
         
-        hplot = new Barplot(p, hplot_props);
-        pplot = new Barplot(p, pplot_props);
-
-        intnet = new Network(p, network_props);
-
-        p.pixelDensity(2);
-
+        p.pixelDensity(3);
     }
     
     p.draw = () => {
         p.background(255);
-
-        // Draw outline
-        p.push();
-        p.fill(0,0,0,0);
-        p.stroke(1);
-        p.strokeWeight(2);
-        p.rect(0,0,canvas_props.width,canvas_props.height);
-        p.pop();
         
-        // Update the population based on simulation
-        pop =  pop.fill(0).map(() => Math.random()/2);
-        // Calculate normalized/marginal population sizes
-        let [host_gpops, para_gpops] = normalized(pop, n_host_types, n_para_types);
+        // Draw outline
+        // p.push();
+        // p.fill(0,0,0,0);
+        // p.stroke(1);
+        // p.strokeWeight(2);
+        // p.rect(0,0,canvas_props.width,canvas_props.height);
+        // p.pop();
 
-        // Update heatmap and redraw
-        hmap.update_data(pop);
-        hmap.draw(p);
+        // Simulation logic
+        if(Math.random()>1.0-trate_slider.get_value()){
+            bacteria.turn_random();
+        }
+        bacteria.move(arena_props);
 
-        // Update host and parasite barplots and redraw
-        hplot.update_data(host_gpops);
-        pplot.update_data(para_gpops);
-        hplot.draw(p);
-        pplot.draw(p);
 
-        // Draw interaction network
-        intnet.draw(p);
+        // Drawing
+        arena.get_agent(agentid).set_x(bacteria.x);
+        arena.get_agent(agentid).set_y(bacteria.y);
+        arena.get_agent(agentid).set_angle(bacteria.angle);
+
+        arena.draw(p);
+        trate_slider.draw(p);
 
         // Draw the framerate
         p.text(p.frameRate(),0,10);
